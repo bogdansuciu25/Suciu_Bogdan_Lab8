36e9b27d71769afaa7792d88bb4e8e3f930bbe3e
Bogdan Suciu <bogdan.suciu25@yahoo.com>
2021-01-08 14:29:35 +0200

    Add project files.

---
+++ Data/Suciu_Bogdan_Lab8Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Suciu_Bogdan_Lab8.Models;
+
+namespace Suciu_Bogdan_Lab8.Data
+{
+    public class Suciu_Bogdan_Lab8Context : DbContext
+    {
+        public Suciu_Bogdan_Lab8Context (DbContextOptions<Suciu_Bogdan_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Suciu_Bogdan_Lab8.Models.Book> Book { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Suciu_Bogdan_Lab8.Data;
+using Suciu_Bogdan_Lab8.Models;
+
+namespace Suciu_Bogdan_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Suciu_Bogdan_Lab8.Data.Suciu_Bogdan_Lab8Context _context;
+
+        public IndexModel(Suciu_Bogdan_Lab8.Data.Suciu_Bogdan_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.ToListAsync();
+        }
+    }
+}
